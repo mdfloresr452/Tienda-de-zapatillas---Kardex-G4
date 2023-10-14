dcb7c24307bc238942580a512f78dfefe9577dde
mdfloresr452 <134460930+mdfloresr452@users.noreply.github.com>
2023-10-14 17:21:55 -0500

    Add files via upload

---
+++ navbar.php
@@ -0,0 +1,6 @@
+<div class="navbar">
+    <a href="create.php">Crear Usuario</a>
+    <a href="read.php">Ver Usuarios</a>
+    <a href="update.php">Actualizar Usuario</a>
+    <a href="delete.php">Borrar Usuario</a>
+</div>
